dcb20bb911d5c7d2a836b793cc2a05b51022793b
Jasonwang911 <396689144@qq.com>
2020-03-23 21:42:12 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<!--
+ * @Author: Jason wang
+ * @Date: 2020-03-23 10:41:56
+ * @Descripttion: 
+ * @version: 
+ -->
+<template>
+  <div id="app">
+    <ul>
+      <li v-for="item in users" :key="item.id">{{item.name}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      users: []
+    }
+  },
+  mounted() {
+    axios.get('https://walnuts.wangshen.top/api/users').then(res => {
+      this.users = res.data
+    })
+  }
+}
+</script>
+
+<style>
+</style>
